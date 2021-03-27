c07eef6fa5d43cd19652069a09dcb983dd4ee08a
Kyeongmin Choi <bombard1004@gmail.com>
2021-03-27 23:07:14 +0900

    Update vec2.cpp

--- vec2.cpp
+++ vec2.cpp
@@ -1,28 +1,40 @@
 // vec2.cpp
 // author bombard1004
-// last_update Jan 31 2021
+// last_update Mar 27 2021
 
 #include <bits/stdc++.h>
 
 class Vec2 {
 private:
-    double x, y;
+    double _x, _y;
 
 public:
-    Vec2(double _x, double _y) {
-        x = _x;
-        y = _y;
+    Vec2(const double &x_in, const double &y_in): _x(x_in), _y(y_in) {}
+
+    double x() const {return _x;}
+    double y() const {return _y;}
+
+    Vec2 operator + (const Vec2 &rhs) const {
+        return Vec2(_x + rhs._x, _y + rhs._y);
     }
 
-    Vec2 operator + (const Vec2 &rhs) {
-        return Vec2(x + rhs.x, y + rhs.y);
+    Vec2 operator - (const Vec2 &rhs) const {
+        return Vec2(_x - rhs._x, _y - rhs._y);
     }
 
-    Vec2 operator - (const Vec2 &rhs) {
-        return Vec2(x - rhs.x, y - rhs.y);
+    Vec2 operator / (const double &d) const {
+        return Vec2(_x/d, _y/d);
     }
 
-    double cross(const Vec2 &rhs) {
-        return x * rhs.y - rhs.x * y;
+    double cross(const Vec2 &rhs) const {
+        return _x * rhs._y - rhs._x * _y;
+    }
+
+    void rotate(double angle, bool rad = true) {
+        if(!rad)
+            angle = acos(-1) / 180 * angle;
+        double rotated_x = _x * cos(angle) - _y * sin(angle);
+        double rotated_y = _x * sin(angle) + _y * cos(angle);
+        _x = rotated_x; _y = rotated_y;
     }
 };
